3fb0ef655b975cc76b4e975ca1aed6d8eb8de54d
Joy <kokimalinda@gmail.com>
2023-03-23 13:47:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let cardContent = [];
+    for(let i = 0; i < names.length; i++ ) {
+        cardContent.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return cardContent;
+}
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
